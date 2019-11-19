ec0831c4a77e9a9dd2c261b9d8e974bac208b60f
teruya <teru0x01.sheep@gmail.com>
2019-11-19 19:10:28 +0900

    add ex script

---
+++ dist/experiment.sh
@@ -0,0 +1,13 @@
+set -eux
+
+ssh lab2 sudo tc qdisc add dev enp0s25 root handle 1:0 tbf rate 20mbit burst 80kb limit 800kb
+ssh lab2 sudo tc qdisc add dev enp0s25 parent handle 1:0 tbf rate 20mbit burst 80kb limit 800kb
+
+loss_rates=(0.08 0.16 0.32 0.64 1.28 2.56 5.12)
+for i in "${loss_rates[@]}"; do
+    ssh lab2 sudo tc qdisc add dev enp0s25 parent 1:1 handle 10:1 netem loss $i%
+    python main.py --MPD https://dash.localdomain:6666/bunny_2s/bbb.mpd -p basic -pro h3
+    python main.py --MPD https://dash.localdomain:4443/bunny_2s/bbb.mpd -p basic
+    ssh lab2 sudo tc qdisc del dev enp0s25 parent 1:1
+done
+
