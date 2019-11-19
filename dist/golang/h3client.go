55e8bfb606e49a8603cf44be3db06c805e6df0aa
teruya <teru0x01.sheep@gmail.com>
2019-11-19 14:56:48 +0900

    c style io

---
+++ dist/golang/h3client.go
@@ -0,0 +1,60 @@
+package main
+
+import "C"
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net/http"
+	"unsafe"
+
+	"github.com/lucas-clemente/quic-go/http3"
+)
+
+var hclient http.Client
+
+func h3client(addr string) []byte {
+	pool, err := x509.SystemCertPool()
+	if err != nil {
+		panic(err)
+	}
+
+	roundTripper := &http3.RoundTripper{
+		TLSClientConfig: &tls.Config{
+			RootCAs:            pool,
+			InsecureSkipVerify: true,
+		},
+	}
+	hclient = http.Client{
+		Transport: roundTripper,
+	}
+	fmt.Printf("GET %s\n", addr)
+	rsp, err := hclient.Get(addr) // 以前のコネクションを使いまわしたい
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("GET response %s: %#v\n", addr, rsp)
+	rsp, _ = hclient.Get(addr)
+	fmt.Printf("GET response %s: %#v\n", addr, rsp)
+
+	body := &bytes.Buffer{}
+	_, err = io.Copy(body, rsp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return body.Bytes()
+}
+
+//export H3client
+func H3client(addr *C.char) unsafe.Pointer {
+	s := C.GoString(addr)
+	resp := h3client(s)
+	length := make([]byte, 8)
+	binary.LittleEndian.PutUint64(length, uint64(len(resp)))
+	return C.CBytes(append(length, resp...))
+}
+
+func main() {}
