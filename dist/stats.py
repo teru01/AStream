5b24275d6a55896b47a61e96ae172d979c5853b8
ono <teru0x01.sheep@gmail.com>
2019-11-29 13:02:18 +0900

    update stat script

--- dist/stats.py
+++ dist/stats.py
@@ -6,10 +6,11 @@ import glob
 from argparse import ArgumentParser
 import os
 
-def get_option(log_file, ssim_file):
+def get_option(log_file, ssim_file, n):
     argparser = ArgumentParser()
     argparser.add_argument('-l', '--logFile', type=str, default=log_file)
     argparser.add_argument('-s', '--ssimFile', type=str, default=ssim_file)
+    argparser.add_argument('-n', '--num', type=int, default=n)
     return argparser.parse_args()
 
 def parse_maximum_layer(log_dict, n):
@@ -34,42 +35,50 @@ def calc_bufratio(log_dict):
     total_stall_duration = log_dict['playback_info']['interruptions']['total_duration']
     return total_stall_duration / (play_duration + total_stall_duration)
 
-def print_layer(log_dict):
+def make_layer_str(log_dict):
     n = log_dict['segment_number']
     layer_of_segments = parse_maximum_layer(log_dict, n)
+    s = ''
     for h in reversed(range(4)):
         for layer in layer_of_segments:
             if layer >= h:
-                print('#', end="")
+                s += '#'
             else:
-                print(' ', end="")
-        print("\n", end="")
+                s += ' '
+        s += "\n"
     for i in range(n):
         if i % 10 == 0:
-            print('.', end="")
+            s += '.'
         else:
-            print(' ', end="")
-    print("\n", end="")
+            s += ' '
+    s += "\n"
+    return s
+
+def generate_stat(logFile, ssimFile):
+        print("logfile: ", logFile)
+        print("ssimfile: ", ssimFile)
+
+
+        with open(logFile, 'r') as f:
+            log_dict = json.loads(f.readline(), object_pairs_hook=OrderedDict)
+
+        with open(ssimFile, 'r') as f_ssim:
+            ssim_dict = json.loads(f_ssim.readline())
+
+        with open('{}./results/result_{}'.format(current_dir, logFile.split('/')[2].replace('json', 'txt')), 'w') as f_result:
+            f_result.write("bufratio: {}\n".format(calc_bufratio(log_dict)))
+            f_result.write("average ssim: {}\n".format(calc_average_ssim(log_dict, ssim_dict)))
+            f_result.write(make_layer_str(log_dict))
+
+
+current_dir = os.path.dirname(__file__)
 
 def main():
-    current_dir = os.path.dirname(__file__)
-    print(__file__)
-    logfile = sorted(glob.glob(current_dir + "./ASTREAM_LOGS/ASTREAM_*"))[-1]
-    ssimfile = sorted(glob.glob(current_dir + "./ssims/ssim_*"))[-1]
-    args = get_option(logfile, ssimfile)
+    logfiles = sorted(glob.glob(current_dir + "./ASTREAM_LOGS/ASTREAM_*"))
+    ssimfiles = sorted(glob.glob(current_dir + "./ssims/ssim_*"))
+    args = get_option(logfiles[-1], ssimfiles[-1], 1)
+    generate_stat(args.logFile, args.ssimFile)
 
-    print("logfile: ", args.logFile)
-    print("ssimfile: ", args.ssimFile)
-
-    with open(args.logFile, 'r') as f:
-        log_dict = json.loads(f.readline(), object_pairs_hook=OrderedDict)
-
-    with open(args.ssimFile, 'r') as f_ssim:
-        ssim_dict = json.loads(f_ssim.readline())
-
-    print("bufratio: ", calc_bufratio(log_dict))
-    print("average ssim: ", calc_average_ssim(log_dict, ssim_dict))
-    print_layer(log_dict)
 
 if __name__ == "__main__":
     main()
