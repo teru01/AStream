f4409d9cf026dc4f5fae517267ca520ca316c837
ono <teru0x01.sheep@gmail.com>
2019-11-30 18:07:12 +0900

    h2 h3 performance test

--- dist/golang/h3test.py
+++ dist/golang/h3test.py
@@ -1,20 +1,34 @@
 import urllib
 from ctypes import *
 import sys
-
+import threading
+import time
 
 lib = cdll.LoadLibrary("./h3client.so")
 client = lib.H3client
-url = "https://dash.localdomain:6666/index.html"
+url = "https://dash.localdomain:6666/fall.jpg"
 
 client.argtypes = [c_char_p]
 client.restype = POINTER(c_ubyte*8)
 # addr = GoString(url.encode('utf8'), len(url))
-
-for i in range(2):
+def download():
     ptr = client(url.encode('utf8'))
     length = int.from_bytes(ptr.contents, byteorder="little")
     data = bytes(cast(ptr,
             POINTER(c_ubyte*(8 + length))
             ).contents[8:])
-    print(data)
+
+def main():
+    start = time.time()
+    threads = []
+    for i in range(50):
+        t = threading.Thread(target=download)
+        threads.append(t)
+        t.start()
+        # print(data)
+    for t in threads:
+        t.join()
+    print(time.time() - start)
+
+if __name__ == "__main__":
+    main()
