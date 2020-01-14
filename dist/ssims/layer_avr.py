eae98f515d5e77e36b0f5d061e59a7c7d23c5a67
ono <teru0x01.sheep@gmail.com>
2020-01-14 12:53:57 +0900

    fix base layer average

--- dist/ssims/layer_avr.py
+++ dist/ssims/layer_avr.py
@@ -1,12 +1,12 @@
 import json
 
 
-with open('ssim_BBB.json') as f:
+with open('ssim_frame_BBB.json') as f:
     data = json.loads(f.readline())
     res = 0
     c = 0
-    for item in data:
-        if 'L0' in item:
-            c += 1
-            res += item['L0']
+    for item in data[:90 * 48]:
+        c += 1
+        res += item[0]
+    print(c)
     print(res / c)
